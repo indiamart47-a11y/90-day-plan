b299311a6e1e220c41778bd9c14e41ba661401cc
indiamart47-a11y <indiamart47@gmail.com>
2026-06-26 12:21:43 +0530

    initial commit

---
+++ type conversion.py
@@ -0,0 +1,126 @@
+#implicit type conversion
+a=10
+b=10.3
+c=a+b
+print(c)
+print(type(c))
+
+#type casting
+a="10"
+a=int(a)
+print(a)
+print(type(a))
+
+#chr()
+a=65
+b=chr(a)
+print(b)
+
+#ord()
+a="A"
+b=ord(a)
+print(b)
+
+#assignemnt operator add and asign
+a=10
+b=20
+a+=b
+print(a)
+
+#subtract and assign
+a=10
+b=20
+a-=b
+print(a)
+
+#muliply and assign
+a=10
+b=20
+a*=b
+print(a)
+
+#divison and assign
+a=10
+b=20
+a/=b
+print(a)
+
+#modulo and assign
+a=10
+b=20
+a%=b
+print(a)
+
+#floor division and assign
+a=10
+b=20
+a//=b
+print(a)
+
+#exponent and assign
+a=2
+b=3
+a**=b
+print(a)
+
+#comparsion operator ==,!=,>=,>,<,<=
+a=10
+b=20
+c=a==b
+print(c)
+
+#!=
+a=10
+b=20
+c=a!=b
+print(c)
+
+#>=
+a=10
+b=20
+c=a>=b
+print(c)
+
+#<=
+a=10
+b=20
+c=a<=b
+print(c)
+
+#membership operators used in list in,not in 
+a=[1,2,3,4]
+b=1 in a
+print(b)
+
+#not in
+a=[1,2,3,4]
+b=1 not in a
+print(b)
+
+#bitwise operators &
+a=3
+b=2
+print(a&b)
+
+#| or operator
+a=3
+b=2
+print(a|b)
+
+#left shift operator <<<
+a=2 << 3
+print(a)
+
+#right shift o-erator
+b=32 >> 2
+print(b)
+
+
+
+
+
+
+
+
+
+
